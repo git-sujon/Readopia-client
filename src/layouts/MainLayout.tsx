3d11fb21b8f9682590554cf8547faf3a14736688
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-07-18 21:44:44 +0600

    Footer Added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,16 @@
-import Navbar from './Navbar';
-import { Outlet } from 'react-router-dom';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar/>
-           <div className='px-0 md:px-10 py-10 md:py-20'>
-           <Outlet/>
-           </div>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <div className="px-0 md:px-10 py-10 md:py-20">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
